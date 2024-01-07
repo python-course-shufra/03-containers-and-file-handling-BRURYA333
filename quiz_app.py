e6db1b82a629006efd491ca1a31ac3832dcacac5
יוסי וברוריה טננבוים <brurya333@gmail.com>
2024-01-07 02:08:12 +0200

    תיקונים בקוד

--- quiz_app.py
+++ quiz_app.py
@@ -1,21 +1,56 @@
 import sys
 import random
+import itertools
 
+
+def create_question(text):
+    text = text.strip()
+    question, correct_answer, options_str = text.split(";")
+    
+    options = options_str.split(',')
+    options.append(correct_answer)
+    random.shuffle(options)
+
+    print(question)
+
+    correct_index = None
+    for i, item in enumerate(options, start=1):
+        print(f'{i}. {item}')
+
+        if item == correct_answer:
+            correct_index = i
+
+    return question, options, correct_index
+
+
+
+def isGood(answer, correct_index):
+     if answer == correct_index:
+        return 1
+     else: return 0
 
 def main():
-    # TODO: your code here
+    profession = sys.argv[1]
+    num_of_question = int(sys.argv[2])
 
-    # 1. Get the command line arguments via sys.argv
+    with open(f'questions/{profession}.txt', mode='r', encoding='utf-8') as read_file:
+        numGoodAnswers = 0
 
-    # 2. Open the correct file open(rf'questions\<filename>.txt)'
+        for line in itertools.islice(read_file, num_of_question):
+            question, options, correct_index = create_question(line)
+            answer = int(input())
+            numGoodAnswers += isGood(answer, correct_index)
+        
 
-    # 3. Iterate over the file
-
-    #       3.1. Parse the line (use s.split())
-    #       3.2 Create a list of options
-    #       3.3 random.shuffle(l)
-    pass
-
+        print(f'You answered {numGoodAnswers} / {num_of_question} good answers!!!')
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
